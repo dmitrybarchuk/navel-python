df0ae64afd38873dfd08b4d0263aadad0cbf9f40
dmitry.barchuk <dmitry.barchuk@gmail.com>
2017-04-07 21:23:32 +0600

    remove httpresponse and loader

--- navel/catalogue/views.py
+++ navel/catalogue/views.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.http import HttpResponse
-from django.template import loader
 from django.shortcuts import render
 from models import Conditioner
 
